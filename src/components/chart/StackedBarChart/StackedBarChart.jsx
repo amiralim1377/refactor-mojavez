4a880e9509162cecb0bd183d5307bf87cd1c24ac
AMIRALI <alimoradi7718@gmail.com>
2025-11-26 14:42:08 +0330

    remove some boilerplate code

--- src/components/chart/StackedBarChart/StackedBarChart.jsx
+++ src/components/chart/StackedBarChart/StackedBarChart.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactECharts from "echarts-for-react";
 import { vazirmatn } from "@/app/fonts/fonts";
 
